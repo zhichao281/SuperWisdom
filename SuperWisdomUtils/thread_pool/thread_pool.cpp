1e5882e5b6f2ebe2d2b1ce775344d8af2aa7ce68
郑智超 <zhichao281@163.com>
2019-12-17 09:22:16 +0800

    *xiugai 线程池

--- SuperWisdomUtils/thread_pool/thread_pool.cpp
+++ SuperWisdomUtils/thread_pool/thread_pool.cpp
@@ -3,15 +3,10 @@
 
 #include "thread_pool.h"
 #include <stdio.h>
-#include <thread>
-#include <mutex>
-#include <memory>
-#include <functional>
 #include <windows.h>
 #include <iostream>
+
 using namespace netlib;
-
-
 
 ThreadPool* ThreadPool::s_pThreadPool = nullptr;
 
@@ -20,12 +15,13 @@ ThreadPool::ThreadPool(int threadNumber)
 m_bRunning(true),
 m_vecThread(m_nThreadNumber)
 {
+
 	m_vecThread.clear();
 }
 
 ThreadPool::~ThreadPool()
 {
-	if (m_bRunning)
+	if (m_bRunning.load())
 	{
 		stop();
 	}
@@ -33,6 +29,7 @@ ThreadPool::~ThreadPool()
 
 bool ThreadPool::start(void)
 {
+	m_bRunning.store(true);
 	for (int i = 0; i < m_nThreadNumber; i++)
 	{
 		m_vecThread.push_back(std::make_shared<std::thread>(std::bind(&ThreadPool::threadWork, this)));//循环创建线程       
@@ -42,15 +39,16 @@ bool ThreadPool::start(void)
 
 bool ThreadPool::stop(void)
 {
-	if (m_bRunning)
+	if (m_bRunning.load())
 	{
-		m_bRunning = false;
+		m_bRunning.store(false);
 		m_condition_empty.notify_all();
-		for (auto t : m_vecThread)
+		//Sleep(100);
+		for (auto & thread : m_vecThread)
 		{
-			if (t->joinable())
+			if (thread->joinable())
 			{
-				t->join();  //循环等待线程终止		
+				thread->detach();
 			}
 		}
 	}
@@ -59,20 +57,20 @@ bool ThreadPool::stop(void)
 
 bool ThreadPool::IsRunning()
 {
-	return m_bRunning;
+	return m_bRunning.load();
 }
 
 ThreadPool * netlib::ThreadPool::GetInstance()
 {
-
+	
 	if (NULL != s_pThreadPool)
 	{
 		return s_pThreadPool;
 	}
 
-	s_pThreadPool = new ThreadPool(3);
+	s_pThreadPool = new ThreadPool(MAX_THREAD);
 	s_pThreadPool->start();
-	//atexit(Destory);
+	atexit(Destory);
 	return s_pThreadPool;
 
 
@@ -88,45 +86,49 @@ bPriority	是否要优先执行
 
 *	Return:			true 成功 false 失败
 */
-bool ThreadPool::append(ThreadTask task, bool bPriority /* = false */)
+bool ThreadPool::append(ThreadTask task,  bool bPriority /* = false */)
 {
-	if (m_bRunning == false)
+	if (m_bRunning.load() == false)
 	{
 		return false;
 	}
 	std::lock_guard<std::mutex> guard(m_mutex);
-	m_taskList.push_front(task);   //将该任务加入任务队列
+	m_taskList.emplace(task);   //将该任务加入任务队列
 	m_condition_empty.notify_one();//唤醒某个线程来执行此任务
 	return true;
+}
+
+ThreadTask ThreadPool::get_one_task()
+{
+
+	ThreadTask task = nullptr;
+	std::unique_lock<std::mutex> _lock(m_mutex);
+	if (m_taskList.empty())
+	{
+		m_condition_empty.wait(_lock);  //等待有任务到来被唤醒
+	}
+	if (!m_taskList.empty() && m_bRunning.load())
+	{
+		task=std::move(m_taskList.front());  //从任务队列中获取最开始任务
+		m_taskList.pop();     //将取走的任务弹出任务队列			
+	}
+	return task;
+
 }
 
 void ThreadPool::threadWork(void)
 {
 	ThreadTask task = nullptr;
-	while (m_bRunning)
+	while (m_bRunning.load())
 	{
-		{
-			std::unique_lock<std::mutex> _lock(m_mutex);
-			task = nullptr;
-			if (m_taskList.empty())
-			{
-				m_condition_empty.wait(_lock);  //等待有任务到来被唤醒
-
-			}
-			if (!m_taskList.empty())
-			{
-				task = m_taskList.front();  //从任务队列中获取最开始任务
-				m_taskList.pop_front();     //将取走的任务弹出任务队列			
-			}
-			else
-			{
-				continue;
-			}
+		{			
+			task = get_one_task();
 		}
-		if (task != nullptr && m_bRunning)
+		if (task != nullptr && m_bRunning.load())
 		{
 			task(); //执行任务
-		}
+			
+		}	
 	}
 	return;
 }
--- SuperWisdomUtils/thread_pool/thread_pool.h
+++ SuperWisdomUtils/thread_pool/thread_pool.h
@@ -4,19 +4,25 @@
 #include <thread>
 #include <mutex>
 #include <condition_variable>
-#include <list>
+#include <queue>
 #include <vector>
 #include <memory>
 #include <functional>
+#include <atomic>
+#include <future>
+
+#include <stdexcept>
 
 
-
+#define MAX_THREAD  10
 namespace netlib
 {
+	using ThreadTask = std::function<void(void)>;
+	
 	class ThreadPool
 	{
-	public:
-		typedef std::function<void(void)> ThreadTask;
+	public:		
+		//typedef std::function<void(void )> ThreadTask;
 
 		ThreadPool(int threadNumber);
 
@@ -24,6 +30,8 @@ namespace netlib
 
 		//往任务队列里添加任务
 		bool append(ThreadTask task, bool bPriority = false);
+
+		ThreadTask get_one_task();
 
 		//启动线程池
 		bool start(void);
@@ -33,7 +41,7 @@ namespace netlib
 
 		// 线程池是否在运行
 		bool IsRunning();
-
+		
 
 		// 获取线程池实例
 		static ThreadPool* GetInstance();
@@ -46,24 +54,32 @@ namespace netlib
 				s_pThreadPool = nullptr;
 			}
 		};
-	public:
+public:
 
 		static ThreadPool*		s_pThreadPool;
-	private:
+private:
 		//线程所执行的工作函数
 		void threadWork(void);
 
-	private:
+private:
 		std::mutex m_mutex;                                        //互斥锁
-
-		volatile bool m_bRunning;                                           //线程池是否在运行
+		
+		std::atomic< bool> m_bRunning;                              //线程池是否在运行
 		int m_nThreadNumber;                                       //线程数
 
 		std::condition_variable_any m_condition_empty;             //当任务队列为空时的条件变量
-		std::list<ThreadTask> m_taskList;                          //任务队列
+		std::queue<ThreadTask> m_taskList;                          //任务队列
 		std::vector<std::shared_ptr<std::thread>> m_vecThread;     //用来保存线程对象指针
+		//空闲线程数量
+		std::atomic<int>  m_idlThrNum;
+
 	};
 }
 
 #define GSThreadPool_AddFun(x)	netlib::ThreadPool::GetInstance()->append(x)
+
 #define GSThreadPool	netlib::ThreadPool::GetInstance()
+
+
+
+
